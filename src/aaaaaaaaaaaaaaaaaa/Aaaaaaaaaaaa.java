516ef9470a6352d9b274f6674963a60a2c766ec3
prudhommepatwit <prudhommep@wit.edu>
2023-10-02 17:47:40 -0400

    Added da whole project
    I have added the Java project to the repository, asking for your favorite day of the week

---
+++ src/aaaaaaaaaaaaaaaaaa/Aaaaaaaaaaaa.java
@@ -0,0 +1,17 @@
+package aaaaaaaaaaaaaaaaaa;
+
+import java.util.Scanner;
+
+public class Aaaaaaaaaaaa {
+	public Aaaaaaaaaaaa() {
+		// TODO Auto-generated constructor stub
+		}
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Whatever you want, I just want output and to know that you can make your own java program");
+		System.out.print("What is your favorite day of the week? ");
+		String favoriteDay = in.next();
+		System.out.print("Wow, I am so glad that your favorite day of the week is "+favoriteDay);
+	}
+
+}
